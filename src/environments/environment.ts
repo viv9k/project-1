2cea7db3d2105c10967835ed317828bf56921322
vivek kumar <vivekkumar@viveks-MacBook-Air-2.local>
2021-04-11 22:09:31 +0530

    fix(deployement): Deployment fix
    Signed-off-by: vivek kumar <vivekkumar@viveks-MacBook-Air-2.local>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-
+    apiKey: "AIzaSyDfwW9b7R_Nk49zE4mO9kUC9X1ZmrF_UYg",
+  authDomain: "vh-ecom.firebaseapp.com",
+  projectId: "vh-ecom",
+  storageBucket: "vh-ecom.appspot.com",
+  messagingSenderId: "657918131668",
+  appId: "1:657918131668:web:553df723f0160be4466a41",
+  measurementId: "G-KW1GD5DNDT"
   },
   useEmulators: true
 };
